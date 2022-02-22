c6be7552cfec0999c58229fb67abc3d47d78d882
Konstantin Barnaev <konstantin.barnaev@gmail.com>
2022-02-22 22:24:47 +0300

    Тоже рабочий вариант с преобразованием JSON в объект, но уже через OkHttp

--- app/src/main/java/com/example/first_connection/MainActivity.kt
+++ app/src/main/java/com/example/first_connection/MainActivity.kt
@@ -3,9 +3,12 @@ package com.example.first_connection
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.google.gson.Gson
+import okhttp3.*
 import java.io.BufferedReader
+import java.io.IOException
 import java.io.InputStreamReader
 import java.net.URL
+import java.sql.SQLOutput
 import java.util.concurrent.Executors
 import javax.net.ssl.HttpsURLConnection
 
@@ -13,20 +16,35 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val client = OkHttpClient()
+        println("Клиент OkHttp создан")
+        val request = Request.Builder()
+            .url("https://reqres.in/api/users/2")
+            .build()
+        println("Request.Builder создан")
 
-        Executors.newSingleThreadExecutor().execute {
-            val url = URL("https://reqres.in/api/users/2")
-            val connection = url.openConnection() as HttpsURLConnection
-            val gson = Gson () // создаём экземпляр класса, чтобы воспользоваться его методами
-            try {
-                //connection.setRequestProperty("User-Agent", "") //костыль от А.Солдатенкова
-                val br = BufferedReader(InputStreamReader(connection.inputStream))
-                val line = br.readLine()
-                val pers = gson.fromJson(line, Person::class.java)
-                println("!!! ${pers.data}")
-            } finally {
-                connection.disconnect()
+//Создаем отправку запроса, мы должны имплементировать интерфейс Callback,
+//когда будете его импортировать, проверьте, чтобы он был от библиотеки OkHttp, потому что есть
+//Интерфейсы с таким же названием и в других библиотеках
+        client.newCall(request).enqueue(object : Callback {
+
+            //Переопределяем метод, что будет, если мы не сможем получить ответ на запрос
+            override fun onFailure(call: Call, e: IOException) {
+                e.printStackTrace()
+                println("Ответ НЕ получен")
             }
-        }
+            //Переопределяем метод, что будет, если мы сможем получить ответ на запрос
+            override fun onResponse(call: Call, response: Response) {
+                //Здесь тоже надо обернуть в try-catch
+                try {
+                    println(API.Key)
+                    val responseBody = response.body()
+                    println("!!! ${responseBody?.string()}")
+                } catch (e: Exception) {
+                    println(response)
+                    e.printStackTrace()
+                }
+            }
+        })
     }
 }
